b2f40b62f7f0826b07baf88c68f0b2e49f20182c
Mike Zahalan <mzahalan@amazon.com>
2025-07-09 14:20:39 -0700

    Lesson 44 - submit handling
    On Submit - we create a new character and add it to the array of allCharacters.
    
    - I don't really like that allCharacters is a global defined in some random file, but I think we change that later.
    
    I also had a typo where slogan was typed 'slogal' I fixed that everywhere.

--- flutter_rpg/lib/models/character.dart
+++ flutter_rpg/lib/models/character.dart
@@ -6,7 +6,7 @@ class Character with Stats {
 
   Character({
     required this.name,
-    required this.slogal,
+    required this.slogan,
     required this.vocation,
     required this.id
   });
@@ -14,7 +14,7 @@ class Character with Stats {
   final Vocation vocation;
   final Set<Skill> skills = {};
   final String name;
-  final String slogal;
+  final String slogan;
   final String id;
 
   bool _isFav = false;
@@ -33,26 +33,26 @@ class Character with Stats {
 
 List<Character> allCharacters = [
   // Raider Characters
-  Character(name: 'Kael', slogal: 'Swift as wind, sharp as steel', vocation: Vocation.raider, id: 'raider_01'),
-  Character(name: 'Vera', slogal: 'Dance with death, emerge victorious', vocation: Vocation.raider, id: 'raider_02'),
+  Character(name: 'Kael', slogan: 'Swift as wind, sharp as steel', vocation: Vocation.raider, id: 'raider_01'),
+  Character(name: 'Vera', slogan: 'Dance with death, emerge victorious', vocation: Vocation.raider, id: 'raider_02'),
   
   // Mage Characters
-  Character(name: 'Zara', slogal: 'Knowledge is the ultimate power', vocation: Vocation.mage, id: 'mage_01'),
-  Character(name: 'Magnus', slogal: 'Ice flows through my veins', vocation: Vocation.mage, id: 'mage_02'),
+  Character(name: 'Zara', slogan: 'Knowledge is the ultimate power', vocation: Vocation.mage, id: 'mage_01'),
+  Character(name: 'Magnus', slogan: 'Ice flows through my veins', vocation: Vocation.mage, id: 'mage_02'),
   
   // Warrior Characters
-  Character(name: 'Thorin', slogal: 'Strength conquers all obstacles', vocation: Vocation.warrior, id: 'warrior_01'),
-  Character(name: 'Lyra', slogal: 'Fear is for the weak', vocation: Vocation.warrior, id: 'warrior_02'),
+  Character(name: 'Thorin', slogan: 'Strength conquers all obstacles', vocation: Vocation.warrior, id: 'warrior_01'),
+  Character(name: 'Lyra', slogan: 'Fear is for the weak', vocation: Vocation.warrior, id: 'warrior_02'),
   
   // Archer Characters
-  Character(name: 'Finn', slogal: 'One shot, one kill', vocation: Vocation.archer, id: 'archer_01'),
-  Character(name: 'Aria', slogal: 'Silent death from afar', vocation: Vocation.archer, id: 'archer_02'),
+  Character(name: 'Finn', slogan: 'One shot, one kill', vocation: Vocation.archer, id: 'archer_01'),
+  Character(name: 'Aria', slogan: 'Silent death from afar', vocation: Vocation.archer, id: 'archer_02'),
   
   // Healer Characters
-  Character(name: 'Celeste', slogal: 'Light heals all wounds', vocation: Vocation.healer, id: 'healer_01'),
-  Character(name: 'Sage', slogal: 'Life flows eternal', vocation: Vocation.healer, id: 'healer_02'),
+  Character(name: 'Celeste', slogan: 'Light heals all wounds', vocation: Vocation.healer, id: 'healer_01'),
+  Character(name: 'Sage', slogan: 'Life flows eternal', vocation: Vocation.healer, id: 'healer_02'),
   
   // Rogue Characters
-  Character(name: 'Raven', slogal: 'Death comes from the shadows', vocation: Vocation.rogue, id: 'rogue_01'),
-  Character(name: 'Nyx', slogal: 'Unseen, unheard, unstoppable', vocation: Vocation.rogue, id: 'rogue_02'),
+  Character(name: 'Raven', slogan: 'Death comes from the shadows', vocation: Vocation.rogue, id: 'rogue_01'),
+  Character(name: 'Nyx', slogan: 'Unseen, unheard, unstoppable', vocation: Vocation.rogue, id: 'rogue_02'),
 ];
